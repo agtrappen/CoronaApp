7ddc1c1b79a92f039eedf134496ae84d282e903f
Alexander <agtrappen@hotmail.com>
2020-12-09 21:22:45 +0100

    added pages

--- app/src/main/java/com/example/coronaapp/MainActivity.kt
+++ app/src/main/java/com/example/coronaapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.coronaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_title)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+        return true
+    }
+//
+//    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?): Boolean {
+//        super.onCreateOptionsMenu(menu)
+//        inflater?.inflate(R.menu.overflow_menu, menu)
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+//        return NavigationUI.onNavDestinationSelected(item!!,
+//                view!!.findNavController())
+//                || super.onOptionsItemSelected(item)
+//    }
 }
---
+++ app/src/main/java/com/example/coronaapp/TitleFragment.kt
@@ -0,0 +1,18 @@
+package com.example.coronaapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TitleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_title, container, false)
+    }
+}
