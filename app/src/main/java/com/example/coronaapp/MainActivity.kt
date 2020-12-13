ace85e5522b0748debe07138baa8f1023f3529ab
Alexander <agtrappen@hotmail.com>
2020-12-13 21:01:59 +0100

    fix navigation

--- app/src/main/java/com/example/coronaapp/MainActivity.kt
+++ app/src/main/java/com/example/coronaapp/MainActivity.kt
@@ -6,28 +6,42 @@ import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_symptoms)
+        setContentView(R.layout.fragment_title)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.overflow_menu, menu)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.overflow_menu, menu)
         return true
     }
-//
-//    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?): Boolean {
-//        super.onCreateOptionsMenu(menu)
-//        inflater?.inflate(R.menu.overflow_menu, menu)
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-//        return NavigationUI.onNavDestinationSelected(item!!,
-//                view!!.findNavController())
-//                || super.onOptionsItemSelected(item)
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.homeFragment -> {
+                setContentView(R.layout.fragment_title)
+                true
+            }
+            R.id.symptomsFragment -> {
+                setContentView(R.layout.fragment_symptoms)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        val navController = findNavController(R.id.)
+//        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
 //    }
 }
