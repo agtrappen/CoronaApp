b1be74635d1d307ba62784012645ac0b0dc515c2
Alexander <agtrappen@hotmail.com>
2020-12-20 14:29:11 +0100

    added corona symptoms repository

---
+++ app/src/main/java/com/example/coronaapp/database/CoronaSymptoms.kt
@@ -0,0 +1,14 @@
+package com.example.coronaapp.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "corona_symptoms")
+data class CoronaSymptoms(
+        @PrimaryKey(autoGenerate = true)
+        var symptomId: Long = 0L,
+
+        @ColumnInfo(name = "corona_symptom")
+        var symptom: String
+)
