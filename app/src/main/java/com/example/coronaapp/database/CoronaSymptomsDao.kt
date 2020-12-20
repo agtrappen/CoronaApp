1d68fce8f7519af06b61d28c2c7130bad1ffbcd9
Alexander <agtrappen@hotmail.com>
2020-12-20 14:38:21 +0100

    added corona symptoms dao

--- app/src/main/java/com/example/coronaapp/database/CoronaSymptomsDao.kt
+++ app/src/main/java/com/example/coronaapp/database/CoronaSymptomsDao.kt
@@ -1,2 +1,22 @@
 package com.example.coronaapp.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface CoronaSymptomsDao {
+    @Insert
+    fun insert(symptoms: CoronaSymptoms)
+
+    @Query("SELECT * from corona_symptoms WHERE symptomId = :key")
+    fun get(key: Long): CoronaSymptoms
+
+    @Query("DELETE FROM corona_symptoms")
+    fun clear()
+
+    @Query("SELECT * FROM corona_symptoms ORDER BY symptomId DESC")
+    fun getAllSymptoms(): LiveData<List<CoronaSymptoms>>
+}
