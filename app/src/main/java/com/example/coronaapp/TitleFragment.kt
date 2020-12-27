70ad7f3c6cd651bb3b0df5ebd4cfa06c24b797eb
Alexander <agtrappen@hotmail.com>
2020-12-27 15:53:58 +0100

    added TitleViewModel

--- app/src/main/java/com/example/coronaapp/TitleFragment.kt
+++ app/src/main/java/com/example/coronaapp/TitleFragment.kt
@@ -5,26 +5,25 @@ import android.util.Log
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.coronaapp.databinding.FragmentTitleBindingImpl
 
 class TitleFragment : Fragment() {
+    private val viewModel: TitleViewModel by lazy {
+        ViewModelProvider(this).get(TitleViewModel::class.java)
+    }
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        Log.d("TEST", "HEY")
-//
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_title, container, false)
-//    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBindingImpl = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title,  container, false
         )
+
+        binding.lifecycleOwner = this
+
+        binding.viewModel = viewModel
 
         setHasOptionsMenu(true)
 
