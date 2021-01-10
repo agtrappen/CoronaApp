4e3699f468a321291ee1a6395dea5a0dddf46355
Alexander <agtrappen@hotmail.com>
2021-01-10 20:50:14 +0100

    show advice

--- app/src/main/java/com/example/coronaapp/SymptomsFragment.kt
+++ app/src/main/java/com/example/coronaapp/SymptomsFragment.kt
@@ -1,21 +1,19 @@
 package com.example.coronaapp
 
-import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import android.os.Bundle
-import android.util.Log
 import android.view.*
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
 import com.example.coronaapp.database.CoronaDatabase
 import com.example.coronaapp.databinding.FragmentSymptomsBindingImpl
-import kotlinx.android.synthetic.main.fragment_information.*
+import kotlinx.android.synthetic.main.fragment_symptoms.*
+import java.util.*
 
 class SymptomsFragment : Fragment() {
     private val viewModel: TitleViewModel by lazy {
@@ -35,7 +33,7 @@ class SymptomsFragment : Fragment() {
 
         val symptomsViewModel =
                 ViewModelProvider(this, viewModelFactory).get(SymptomsViewModel::class.java)
-
+        
         binding.symptomsViewModel = symptomsViewModel
 
         binding.setLifecycleOwner(this)
@@ -45,13 +43,62 @@ class SymptomsFragment : Fragment() {
         return binding.root
     }
 
-    fun checkSymptoms() {
-        Log.i("Test", "Testovich")
+    private fun showDialog() {
+        val currentLanguage: String = Locale.getDefault().getDisplayLanguage()
+
+        val dialog = MaterialDialog(requireContext())
+                .noAutoDismiss()
+                .customView(R.layout.result_dialog)
+
+        dialog.findViewById<TextView>(R.id.cancel_button).setOnClickListener {
+            dialog.dismiss()
+        }
+
+        var count = 0
+        if (checkBox.isChecked()) {
+            count++
+        }
+
+        if (checkBox2.isChecked()) {
+            count++
+        }
+
+        if (checkBox3.isChecked()) {
+            count++
+        }
+
+        if (checkBox4.isChecked()) {
+            count++
+        }
+
+        if (count > 2) {
+            if (currentLanguage.toLowerCase().contains("en")) {
+                dialog.findViewById<TextView>(R.id.advice_text).text = "The advice based on your symptoms is that you should be tested for corona. You can do this on the website of the national government by logging in with your digiD or by calling: 0800-1202"
+            } else {
+                dialog.findViewById<TextView>(R.id.advice_text).text = "Het advies op basis van uw ingevulde symptomen luidt dat u zich moeten laten testen op corona. U kunt dit doen op de website van de rijksoverheid door in te loggen met uw digiD of telefonisch contact op te nemen met:0800-1202"
+            }
+        } else {
+            if (currentLanguage.toLowerCase().contains("en")) {
+                dialog.findViewById<TextView>(R.id.advice_text).text = "The advice based on your completed symptoms is that you do not need to be tested for corona."
+            } else {
+                dialog.findViewById<TextView>(R.id.advice_text).text = "Het advies op basis van uw ingevulde symptomen luidt dat u zich niet hoeft te laten testen op corona."
+            }
+        }
+
+
+        dialog.show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        advice_button.setOnClickListener {
+            showDialog()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
